ad4035f03a7c011ddd5361f5bdb47f748bcc6dd5
Igor Dvorkin <idvorkin@gmail.com>
2021-04-18 07:00:21 -0700

    Save gen-unique.py

--- gen-unique.py
+++ gen-unique.py
@@ -10,6 +10,12 @@
 # Sci-Kit Image: https://scikit-image.org/
 # Python Motion Detecor: https://www.geeksforgeeks.org/webcam-motion-detector-python/
 
+
+#######
+# Higher level primitive being used - Background substituion
+############
+#   - https://docs.opencv.org/3.4/d1/dc5/tutorial_background_subtraction.html
+
 from imutils.video import FileVideoStream, FPS
 from imutils import skeletonize
 from icecream import ic
@@ -18,20 +24,31 @@ import cv2
 import numpy as np
 from dataclasses import dataclass
 import copy
+from typing import Optional
+import typer
+from pathlib import Path
+
+app = typer.Typer()
 
 
-backSub = cv.createBackgroundSubtractorMOG2()
-# backSub = cv.createBackgroundSubtractorKNN()
+backSub = cv2.createBackgroundSubtractorMOG2(history=120)
+# backSub = cv2.createBackgroundSubtractorKNN()
+
+# Some globals
+color_black = 0
+color_white = 255
+color_grey = 127
+
 
 @dataclass
 class FrameState:
     idx: any
     frame: any
     last_analysis_frame: any
-    last_analysis_diff: any
+    last_fg_mask: any
 
 
-analysis_fps = 10
+analysis_key_frame_rate = 15
 output_fps = 1000
 
 
@@ -47,48 +64,82 @@ def remove_ring_timestamp(frame):
     return cv2.rectangle(frame, start_point, end_point, color, thickness)
 
 
+def threshold(frame):
+    color_threshold = 127
+    convert_to_color = color_white
+
+    ## QQ: Is this doing anything if I've already bg subtracted - not so sure!
+    ## I guess I can histogram to find out?
+    ret, threshed = cv2.threshold(
+        frame,
+        color_threshold,
+        convert_to_color,
+        cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU,
+    )
+    frame = threshed
+
+    # findContours needs image to contour to be white, so invert threshold
+    # Should be able to do in cv2.threshold, but feeling lazy.
+
+    return cv2.bitwise_not(frame)
+
+
+def square_kernel(side):
+    return np.ones((side, side), np.uint8)
+
+
+def to_contours(frame):
+
+    frame = threshold(frame)
+
+    # Remove artifact noise, 10x10 seems like plenty
+    erode_kernel = square_kernel(10)
+    frame = cv2.erode(frame, square_kernel(10))
+
+    # Without noise, can dialate a fair bit
+    frame = cv2.dilate(frame, square_kernel(40))
+
+    contouring_method = cv2.RETR_EXTERNAL  # Only outer edges
+    # contouring_method =  cv2.RETR_CCOMP # 2 level outer, then inner
+    # contouring_method =  cv2.RETR_FLOODFILL # Fill it in
+
+    contours, hierachy = cv2.findContours(
+        frame, contouring_method, cv2.CHAIN_APPROX_NONE
+    )
+
+    for i, contour in enumerate(contours):
+        if cv2.contourArea(contour) < 100:
+            continue
+        # ic(i)
+
+        ## (4) Create mask and do bitwise-op
+        contour_color = color_grey  # Black
+        contour_index = 0  # drawCounters takes an array and index as input
+        contour_thickness_fill = -1
+        frame = cv2.drawContours(
+            frame, [contour], contour_index, contour_color, contour_thickness_fill
+        )
+
+    # Dialate again to try and fill holes
+    dialate_kernel = square_kernel(200)
+    frame = cv2.dilate(frame, dialate_kernel)
+
+    return frame
+
+
 def analyze(state: FrameState, frame):
-    is_analysis_frame = state.idx % analysis_fps == 0
-    if not is_analysis_frame:
+    is_first_frame = state.idx < 2
+    is_analysis_frame = state.idx % analysis_key_frame_rate == 0
+    is_do_analyze = is_analysis_frame or is_first_frame
+    if not is_do_analyze:
         # to see input video, return frame when not analysis
         # return frame
-        return state.last_analysis_diff
+        return state.last_fg_mask
 
-    is_first_frame = state.idx == 0
-    initialize_state = is_first_frame
-    if initialize_state:
-        state.last_analysis_frame = frame
-        return frame
-
-    blur  = cv2.blur(frame, (5, 5))
-    tmp = cv2.absdiff(blur, state.last_analysis_frame)
-    state.last_analysis_frame =  blur
-
-
-    # tmp = cv2.subtract(skeleton, state.last_analysis_frame)
-    # tmp = np.clip(tmp, 0, None)  # bound array between 0 and None
-
-    # Threshold
-    tmp = cv2.adaptiveThreshold(
-        tmp,
-        255,
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY,
-        11,
-        2
-        # tmp, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2
-    )
-    tmp = cv2.dilate(tmp, None, iterations = 1)
-    kernel = np.ones((200,200),np.uint8)
-    tmp = cv2.morphologyEx(tmp, cv2.MORPH_OPEN, kernel)
-
-
-    # Mask out the motion
-    mask = cv2.bitwise_not(tmp)
-    tmp = cv2.bitwise_and(state.frame,state.frame,mask=mask)
-
-    state.last_analysis_diff = tmp
-    return tmp
+    fgMask = backSub.apply(frame)
+    fgMask = to_contours(fgMask)
+    state.last_fg_mask = fgMask
+    return state.last_fg_mask
 
 
 # Gray Scale Frame
@@ -114,9 +165,10 @@ def process_frame(state: FrameState, frame):
     return frame
 
 
-def main():
-    print("Hello")
-    input_file = "in.mp4"
+def main(input_file):
+    if input_file == None:
+        input_file = "in.mp4"
+
     # out_file = "unique.mp4"
     fvs = FileVideoStream(input_file)
     # start the FPS timer
@@ -126,12 +178,11 @@ def main():
     ic(fvs)
     stream = cv2.VideoCapture(input_file)
     ic(stream.isOpened())
-    state = FrameState(np.ones(1),0, 0, 0)
+    state = FrameState(0, 0, 0, 0)
 
-    name = 'output.mp4'
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    output = cv2.VideoWriter(name, fourcc, 20.0, (1920,1200))
-
+    name = "output.mov"
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    output = cv2.VideoWriter(name, fourcc, 20.0, (1920, 1200))
 
     while True:
         # grab the frame from the threaded video file stream, resize
@@ -147,7 +198,7 @@ def main():
 
         cv2.putText(
             in_frame,
-            f"{state.idx}:{state.last_analysis_diff}",
+            f"{state.idx}:{state.last_fg_mask}",
             (10, 30),
             cv2.FONT_HERSHEY_SIMPLEX,
             0.6,
@@ -156,11 +207,11 @@ def main():
         )
         cv2.putText(
             frame,
-            f"{state.idx}:{state.last_analysis_diff}",
+            f"{state.idx}:{state.last_fg_mask}",
             (10, 30),
             cv2.FONT_HERSHEY_SIMPLEX,
             0.6,
-            (0, 0, 0),
+            (255, 255, 255),
             2,
         )
 
@@ -168,15 +219,18 @@ def main():
             if not isinstance(src, np.ndarray):
                 return src
 
-            #calculate the 50 percent of original dimensions
-            scale_percent=50
+            # calculate the 50 percent of original dimensions
+            scale_percent = 50
             width = int(src.shape[1] * scale_percent / 100)
             height = int(src.shape[0] * scale_percent / 100)
             dsize = (width, height)
             return cv2.resize(src, dsize)
 
         cv2.imshow("Input", resize(in_frame))
-        cv2.imshow("Frame", resize(frame))
+        cv2.imshow("Mask", resize(frame))
+        cv2.imshow(
+            "MaskedInput", resize(cv2.bitwise_and(in_frame, in_frame, mask=frame))
+        )
         output.write(in_frame)
         display_duration_ms = int(
             duration(seconds=1 / output_fps).total_seconds() * 1000
@@ -194,5 +248,12 @@ def main():
     cv2.destroyAllWindows()
     output.release()
 
+@app.command()
+def RemoveBackground(video_input_file: Optional[Path] = typer.Argument(None)) -> None:
+    """
+    Remove background from Ring Video
+    """
+    return main(video_input_file)
 
-main()
+if __name__ == "__main__":
+    app()
